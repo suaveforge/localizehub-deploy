8bdca1bb66f79eb5f1e6f3e67fb1e024b963c042
LocalizeHub Deploy <programmer119@users.noreply.github.com>
2026-08-24 00:40:57 +0900

    LocalizeHub Updated 2026.08.24-1 deploy

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.LOCALIZEHUB_CONFIG = {
   apiBase: 'https://api-localizehub.suaveforge.com:18025',
-  updated: 'Updated 2026.08.23-12',
-  build: '20260823110506'
+  updated: 'Updated 2026.08.24-1',
+  build: '20260823153926'
 };
