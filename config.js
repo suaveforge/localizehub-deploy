24777f8889a5766cd51e0d0d64ecb88a8ab23fd1
SuaveForge server123 deploy <actions@suaveforge.local>
2026-09-04 11:33:39 +0000

    LocalizeHub server123 deploy 021224c72ea2

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.LOCALIZEHUB_CONFIG = {
   apiBase: 'https://api-localizehub.suaveforge.com:18025',
   updated: 'Updated 2026.08.25-5',
-  build: '20260904112929-54cf47114bfa'
+  build: '20260904113306-021224c72ea2'
 };
